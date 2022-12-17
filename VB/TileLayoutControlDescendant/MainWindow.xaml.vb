726aef91d24865e43378af8f5409c9f193c5ae68
DevExpressExampleBot <fakeEmail>
2022-12-17 06:59:51 +0400

    Source auto update [skip ci]

--- VB/TileLayoutControlDescendant/MainWindow.xaml.vb
+++ VB/TileLayoutControlDescendant/MainWindow.xaml.vb
@@ -1,77 +1,67 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
 Imports System.Windows
 Imports System.Windows.Input
 Imports System.Collections.ObjectModel
 Imports DevExpress.Xpf.Grid
 
 Namespace TileLayoutControlDescendant
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Private handler As Integer = GridControl.InvalidRowHandle
-		Private point As Point
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-			desc.ItemsSource = DataHelper.GetData1()
-			gridControl1.ItemsSource = DataHelper.GetData2()
-		End Sub
+        Private handler As Integer = DataControlBase.InvalidRowHandle
 
-		Private Sub Dumping()
-			handler = GridControl.InvalidRowHandle
-			point = New Point(-1000, -1000)
-		End Sub
+        Private point As Point
 
-		Private Sub gridControl1_Drop(ByVal sender As Object, ByVal e As DragEventArgs)
-			If e.Data.GetDataPresent("DragDropDataDG") Then
-				TryCast((TryCast(sender, GridControl)).ItemsSource, ObservableCollection(Of ExampleObject)).Add(DirectCast(e.Data.GetData("DragDropDataDG"), ExampleObject))
-			End If
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.desc.ItemsSource = DataHelper.GetData1()
+            Me.gridControl1.ItemsSource = DataHelper.GetData2()
+        End Sub
 
-		Private Sub gridControl1_PreviewMouseDown(ByVal sender As Object, ByVal e As MouseButtonEventArgs)
-			If e.LeftButton = MouseButtonState.Pressed Then
-				point = e.GetPosition(Nothing)
-				Dim grid As GridControl = TryCast(sender, GridControl)
-				Dim info As GridViewHitInfoBase
-				Dim view As CardView = TryCast(grid.View, CardView)
+        Private Sub Dumping()
+            handler = DataControlBase.InvalidRowHandle
+            point = New Point(-1000, -1000)
+        End Sub
 
-				info = view.CalcHitInfo(TryCast(e.OriginalSource, DependencyObject))
-				If info.RowHandle <> GridControl.InvalidRowHandle Then
-					handler = info.RowHandle
-					e.Handled = True
-				End If
-			End If
-		End Sub
+        Private Sub gridControl1_Drop(ByVal sender As Object, ByVal e As DragEventArgs)
+            If e.Data.GetDataPresent("DragDropDataDG") Then
+                TryCast(TryCast(sender, GridControl).ItemsSource, ObservableCollection(Of ExampleObject)).Add(CType(e.Data.GetData("DragDropDataDG"), ExampleObject))
+            End If
+        End Sub
 
-		Private Sub gridControl1_MouseMove(ByVal sender As Object, ByVal e As MouseEventArgs)
-			Dim grid As GridControl = TryCast(sender, GridControl)
-			Dim view As GridViewBase = TryCast(grid.View, GridViewBase)
-			Dim mousePos As Point = e.GetPosition(Nothing)
+        Private Sub gridControl1_PreviewMouseDown(ByVal sender As Object, ByVal e As MouseButtonEventArgs)
+            If e.LeftButton = MouseButtonState.Pressed Then
+                point = e.GetPosition(Nothing)
+                Dim grid As GridControl = TryCast(sender, GridControl)
+                Dim info As GridViewHitInfoBase
+                Dim view As CardView = TryCast(grid.View, CardView)
+                info = view.CalcHitInfo(TryCast(e.OriginalSource, DependencyObject))
+                If info.RowHandle <> DataControlBase.InvalidRowHandle Then
+                    handler = info.RowHandle
+                    e.Handled = True
+                End If
+            End If
+        End Sub
 
-			If e.LeftButton <> MouseButtonState.Pressed Then
-				Return
-			End If
-			If handler = GridControl.InvalidRowHandle Then
-				Return
-			End If
+        Private Sub gridControl1_MouseMove(ByVal sender As Object, ByVal e As MouseEventArgs)
+            Dim grid As GridControl = TryCast(sender, GridControl)
+            Dim view As GridViewBase = TryCast(grid.View, GridViewBase)
+            Dim mousePos As Point = e.GetPosition(Nothing)
+            If e.LeftButton <> MouseButtonState.Pressed Then Return
+            If handler = DataControlBase.InvalidRowHandle Then Return
+            Dim rect As Rect = New Rect(point.X - SystemParameters.MinimumHorizontalDragDistance, (point.Y - CInt(SystemParameters.MinimumVerticalDragDistance)), SystemParameters.MinimumHorizontalDragDistance * 2, SystemParameters.MinimumVerticalDragDistance * 2)
+            If Not rect.Contains(mousePos) Then
+                Dim dragData As DataObject = New DataObject("DragDropDataGD", TryCast(view.GetRowElementByRowHandle(handler).DataContext, RowData).Row)
+                Dumping()
+                DragDrop.DoDragDrop(grid, dragData, DragDropEffects.Copy)
+            End If
+        End Sub
 
-			Dim rect As New Rect((point.X - SystemParameters.MinimumHorizontalDragDistance), (point.Y - CInt(Math.Truncate(SystemParameters.MinimumVerticalDragDistance))), SystemParameters.MinimumHorizontalDragDistance * 2, SystemParameters.MinimumVerticalDragDistance * 2)
-
-			If Not rect.Contains(mousePos) Then
-				Dim dragData As New DataObject("DragDropDataGD", (TryCast(view.GetRowElementByRowHandle(handler).DataContext, RowData)).Row)
-				Dumping()
-				DragDrop.DoDragDrop(grid, dragData, DragDropEffects.Copy)
-			End If
-		End Sub
-
-		Private Sub desc_Drop(ByVal sender As Object, ByVal e As DragEventArgs)
-			If e.Data.GetDataPresent("DragDropDataGD") Then
-				Dim cntrl As TileLayoutCntrlDescendant = TryCast(sender, TileLayoutCntrlDescendant)
-				TryCast(cntrl.ItemsSource, ObservableCollection(Of ExampleObject)).Add(DirectCast(e.Data.GetData("DragDropDataGD"), ExampleObject))
-			End If
-		End Sub
-	End Class
-
+        Private Sub desc_Drop(ByVal sender As Object, ByVal e As DragEventArgs)
+            If e.Data.GetDataPresent("DragDropDataGD") Then
+                Dim cntrl As TileLayoutCntrlDescendant = TryCast(sender, TileLayoutCntrlDescendant)
+                TryCast(cntrl.ItemsSource, ObservableCollection(Of ExampleObject)).Add(CType(e.Data.GetData("DragDropDataGD"), ExampleObject))
+            End If
+        End Sub
+    End Class
 End Namespace
--- VB/TileLayoutControlDescendant/TileLayoutCntrlDescendant.vb
+++ VB/TileLayoutControlDescendant/TileLayoutCntrlDescendant.vb
@@ -1,53 +1,52 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
 Imports DevExpress.Xpf.LayoutControl
 Imports DevExpress.Xpf.Core
 Imports System.Windows
 Imports DevExpress.Xpf.Core.Native
 
 Namespace TileLayoutControlDescendant
-	Public Class TileLayoutCntrlDescendant
-		Inherits TileLayoutControl
 
-		Public Sub New()
-			MyBase.New()
-		End Sub
-		Protected Overrides Function CreateController() As PanelControllerBase
-			Return New TileLayoutDescendantController(Me)
-		End Function
-	End Class
+    Public Class TileLayoutCntrlDescendant
+        Inherits TileLayoutControl
 
-	Public Class TileLayoutDescendantItemDragAndDropController
-		Inherits TileLayoutItemDragAndDropController
+        Public Sub New()
+            MyBase.New()
+        End Sub
 
-		Public Sub New(ByVal controller As Controller, ByVal startDragPoint As Point, ByVal dragControl As FrameworkElement)
-			MyBase.New(controller, startDragPoint, dragControl)
+        Protected Overrides Function CreateController() As PanelControllerBase
+            Return New TileLayoutDescendantController(Me)
+        End Function
+    End Class
 
-		End Sub
+    Public Class TileLayoutDescendantItemDragAndDropController
+        Inherits TileLayoutItemDragAndDropController
 
-		Public Overrides Sub DragAndDrop(ByVal p As Point)
-			Dim ctrl As FrameworkElement = Me.Controller.Control
-			If Not LayoutHelper.IsPointInsideElementBounds(p, ctrl, New Thickness()) Then
-				DragControl.SetVisible(False)
-				Dim dragData As New DataObject("DragDropDataDG", DragControl.DataContext)
-				DragDrop.DoDragDrop(ctrl, dragData, DragDropEffects.Copy)
-				DragControl.SetVisible(True)
-				Return
-			End If
-			MyBase.DragAndDrop(p)
-		End Sub
-	End Class
+        Public Sub New(ByVal controller As Controller, ByVal startDragPoint As Point, ByVal dragControl As FrameworkElement)
+            MyBase.New(controller, startDragPoint, dragControl)
+        End Sub
 
-	Public Class TileLayoutDescendantController
-		Inherits TileLayoutController
+        Public Overrides Sub DragAndDrop(ByVal p As Point)
+            Dim ctrl As FrameworkElement = Controller.Control
+            If Not LayoutHelper.IsPointInsideElementBounds(p, ctrl, New Thickness()) Then
+                DragControl.SetVisible(False)
+                Dim dragData As DataObject = New DataObject("DragDropDataDG", DragControl.DataContext)
+                DragDrop.DoDragDrop(ctrl, dragData, DragDropEffects.Copy)
+                DragControl.SetVisible(True)
+                Return
+            End If
 
-		Public Sub New(ByVal control As IFlowLayoutControl)
-			MyBase.New(control)
-		End Sub
+            MyBase.DragAndDrop(p)
+        End Sub
+    End Class
 
-		Protected Overrides Function CreateItemDragAndDropControler(ByVal startDragPoint As Point, ByVal dragControl As FrameworkElement) As DragAndDropController
-			Return New TileLayoutDescendantItemDragAndDropController(Me, startDragPoint, dragControl)
-		End Function
-	End Class
+    Public Class TileLayoutDescendantController
+        Inherits TileLayoutController
+
+        Public Sub New(ByVal control As IFlowLayoutControl)
+            MyBase.New(control)
+        End Sub
+
+        Protected Overrides Function CreateItemDragAndDropControler(ByVal startDragPoint As Point, ByVal dragControl As FrameworkElement) As DragAndDropController
+            Return New TileLayoutDescendantItemDragAndDropController(Me, startDragPoint, dragControl)
+        End Function
+    End Class
 End Namespace
